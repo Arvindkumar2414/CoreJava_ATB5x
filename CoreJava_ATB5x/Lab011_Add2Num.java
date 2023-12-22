39337cf97e0a91f357653a420276796360b19663
arvindkumar2414 <arvyku.singh@gmail.com>
2023-12-22 14:01:12 +0530

    My 2nd commit

---
+++ CoreJava_ATB5x/Lab011_Add2Num.java
@@ -0,0 +1,11 @@
+package CoreJava_ATB5x;
+
+public class Lab011_Add2Num {
+    public static void main(String[] args) {
+        int a=20;
+        int b=30;
+        int c= a+b;
+
+        System.out.println("Addition of a and b is : " +c);
+    }
+}
